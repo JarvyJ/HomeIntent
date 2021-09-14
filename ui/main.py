ece2866f7cc073b9f2bd10a7df48fffac3ae88d8
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2021-09-14 09:33:56 -0500

    got the api working as expected

--- ui/main.py
+++ ui/main.py
@@ -40,24 +40,18 @@ async def unicorn_exception_handler(request: Request, exc: ValidationError):
 @app.get("/api/v1/settings", response_model=FullSettings, response_model_exclude_unset=True)
 def get_settings():
     """
-    This endpoint ended up being accidentally obtuse...
-    It does make sense for PUTting the new object back however.
+    The JSON version of `/config/config.yaml`
 
-    For components _with_ settings objets, the key (component name) is always present when
-    getting settings and if it is disabled, the value is `null`.
-    If it is enabled, then the value is the value of the component's config object.
+    For components _with_ settings, if it is enabled,
+    then the value is the component's config object.
+    If it is disabled, the key will not exist.
 
-    For components _without_ settings objects, if the key is present, then it is enabled.
+    For components _without_ settings, if the key is present it is enabled.
     The value should be `null`.
 
-    Yes, this means that `null` has two different meanings.
-    This is mostly an artifact from storing the config as yaml and how it is represented in json.
-
-    There is a helper object (`x-components-without-settings`) in `additionalProperties` that can
-    be used to determine what components do not have settings objects.
-
-    _However_, when updating the config object,
-    you can just omit any keys (and values) that are not enabled.
+    Components with settings are defined in the OpenAPI spec.
+    A list of components without settings are found in
+    a helper object (`x-components-without-settings`) in `additionalProperties`
 
     """
     if not CONFIG_FILE.is_file():
@@ -71,12 +65,11 @@ def get_settings():
         return FullSettings()
 
 
-@app.put("/api/v1/settings")
-def update_settings(settings: FullSettings, response_model_exclude_unset=True):
+@app.put("/api/v1/settings", response_model=FullSettings, response_model_exclude_unset=True)
+def update_settings(settings: FullSettings):
     """
-    You should probably read the GET /api/v1/settings caveat above.
-
-    tl;dr: omit any key/values for things that are not enabled.
+    This endpoint will convert the JSON back to `/config/config.yaml` discarding any default values
+    in an attempt to keep the config file more easily human editable.
     """
     yaml = YAML()
     if CONFIG_FILE.is_file():
