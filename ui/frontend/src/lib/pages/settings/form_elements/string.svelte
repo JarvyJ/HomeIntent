def770d3e0818e9ee31f1d548fb8e4d84c01592e
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2021-09-14 21:27:23 -0500

    swithed to using common form elements

---
+++ ui/frontend/src/lib/pages/settings/form_elements/string.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let title
+  export let description = ""
+  export let format = ""
+  export let example = ""
+  export let value
+
+  import HelpText from "../HelpText.svelte";
+
+  let id = `${title}-id`
+  let inputType
+
+  switch(format) {
+    case "uri":
+      inputType = "url"
+      break
+
+    default:
+      inputType = "text"
+  }
+
+
+  $: if (value.length > 50) {
+    inputType = "longtext"
+  }
+</script>
+
+<div>
+  <label for="{id}" class="font-bold">{title}</label>
+  <HelpText>{description}</HelpText>
+</div>
+
+<!-- have to do this via if/else because otherwise there's an error with 2way binding -->
+{#if inputType === "text"}
+  <input id="{id}" type="text" class="border border-gray-300 rounded-md focus:outline-none p-1.5" bind:value placeholder="{example}">
+{:else if inputType === "url"}
+  <input id="{id}" type="url" class="border border-gray-300 rounded-md focus:outline-none p-1.5" bind:value placeholder="{example}">
+{:else if inputType === "longtext"}
+  <textarea id="story" name="story" class="text-area border border-gray-300 rounded-md focus:outline-none p-1.5"
+  rows="5" cols="40" placeholder="{example}" bind:value></textarea>
+{/if}
