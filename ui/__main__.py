9cd6180556855416dc229db247186cd75c22e3b2
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2021-09-21 18:18:12 -0500

    cleaning up the log a bit

--- ui/__main__.py
+++ ui/__main__.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main():
-    uvicorn.run("main:app", host="0.0.0.0", port=11102, log_level="info", fd=0)
+    uvicorn.run("main:app", host="0.0.0.0", port=11102, log_level="warning", fd=0)
 
 
 if __name__ == "__main__":
