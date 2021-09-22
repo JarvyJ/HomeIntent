2950d322be76ce7f3eb49fad11056cc05bbcd542
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2021-09-21 21:06:22 -0500

    UI: Setting up logs page (#92)
    * can now stream some logs! Going to try using a HTTP log handler to forward logs from Home Intent

---
+++ ui/frontend/src/lib/components/SectionBar.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let title
+</script>
+
+<nav class="flex items-center bg-gray-800 text-gray-50 px-4 py-3 border-b">
+  <span class="font-semibold text-3xl">{title}</span>
+  <slot></slot>
+</nav>
---
+++ ui/rhasspy_api.py
@@ -0,0 +1,1 @@
+../home_intent/rhasspy_api.py
