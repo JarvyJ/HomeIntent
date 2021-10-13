08b9afb088d8dbc27f8cb1bef6c7000f1cc92c7b
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2021-10-13 12:32:11 -0500

    touch sentences.ini to clear out example sentences

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
+
+# clears the default (example) sentences.ini file for folks using the embedded system
+mkdir -p /profiles/en && touch /profiles/en/sentences.ini
+
 /usr/lib/rhasspy/bin/rhasspy-voltron --user-profiles /profiles --profile en &
 supervisord --configuration /usr/src/app/setup/supervisord.conf
