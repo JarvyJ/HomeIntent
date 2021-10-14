fd986397fd708f89d06c7f23d3a7ca33a55ea90e
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2021-10-14 09:41:54 -0500

    timers should now work with 2.5.11 and added a new timer sentence structure

--- home_intent/components/timer/__init__.py
+++ home_intent/components/timer/__init__.py
@@ -31,10 +31,14 @@ class Timer:
     @intents.sentences(
         [
             "time = 0..128",
-            "set timer [(<time>){hours:!int} hours] [(<time>){minutes:!int} minutes] [(<time>){seconds:!int} seconds]",
-            "set timer (<time>){hours:!int} [($partial_time)] hours",
-            "set timer (<time>){minutes:!int} [($partial_time)] minutes",
-            "set timer (<time>){seconds:!int} [($partial_time)] seconds",
+            "set timer [(<time>){hours} hours] [(<time>){minutes} minutes] [(<time>){seconds} seconds]",
+            "set timer (<time>){hours} [($partial_time)] hours",
+            "set timer (<time>){minutes} [($partial_time)] minutes",
+            "set timer (<time>){seconds} [($partial_time)] seconds",
+            "set a [(<time>){hours} hour] [(<time>){minutes} minute] [(<time>){seconds} second] timer",
+            "set a (<time>){hours} [($partial_time)] hour timer",
+            "set a (<time>){minutes} [($partial_time)] minute timer",
+            "set a (<time>){seconds} [($partial_time)] second timer",
         ]
     )
     def set_timer(
