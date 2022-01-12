fd89e7527b9d6398cdc856cb80a48e9e2527024f
oerkel47 <7231822+oerkel47@users.noreply.github.com>
2022-01-12 22:05:03 +0100

    tweak German timer add threequarter fraction

--- home_intent/components/timer/de.py
+++ home_intent/components/timer/de.py
@@ -7,48 +7,35 @@ class Timer(BaseTimer):
         return {
             "einhalb": "half",
             "einviertel": "quarter",
-            "[und] dreiviertel": "threequarters"
-            #"and [a] third": "third", # no one says this
+            "dreiviertel": "threequarters"            
         }
 
     @intents.repeatable_dictionary_slots
     def timer_number(self):
         # this is used to overwrite (or add) some numbers provided by Rhasspy's num2words package
         return {            
+            "eine": "1",
             "hundert": "100",
-            "hunderteins": "101",
-            "hundertzwei": "102",
-            "hundertdrei": "103",
-            "hundertvier": "104",
-            "hundertfünf": "105",
+            "hundertzehn": "110",
+            "hundertzwanzig": "120",
         }
    
-
+    @intents.satellite_id
     @intents.sentences(
         [
-            "time = ($timer_number | 0..120)",
-            "Stell (den | einen) (Wecker | Timer) auf [(<time>){hours} Stunden] [(<time>){minutes} Minuten] [(<time>){seconds} Sekunden]",
-            "Stell (den | einen) (Wecker | Timer)  auf (<time>){hours} [($timer_partial_time)] Stunden",
-            "Stell (den | einen) (Wecker | Timer)  auf (<time>){minutes} [($timer_partial_time)] Minuten",
-            "Stell (den | einen) (Wecker | Timer)  (<time>){seconds} [($timer_partial_time)] Sekunden",
-            "Stell einen [(<time>){hours} Stunden] [(<time>){minutes} Minuten] [(<time>){seconds} Sekunden] (Wecker | Timer)",
+            "time = $timer_number | 0..120",
+            "Stell (den | einen) (Wecker | Timer) auf (<time>){hours} [($timer_partial_time)] Stunden",
+            "Stell (den | einen) (Wecker | Timer) auf (<time>){minutes} [($timer_partial_time)] Minuten",
+            "Stell (den | einen) (Wecker | Timer) auf (<time>){seconds} [($timer_partial_time)] Sekunden",
+            "Stell (den | einen) (Wecker | Timer) auf (<time>){hours} Stunden und (<time>){minutes} Minuten",
+            "Stell (den | einen) (Wecker | Timer) auf (<time>){minutes} Minuten und (<time>){seconds} Sekunden",
+            "Stell einen (<time>){hours} Stunden und (<time>){minutes} Minuten Timer",
+            "Stell einen (<time>){minutes} Minuten und (<time>){seconds} Sekunden Timer",
             "Stell einen (<time>){hours} [($timer_partial_time)] Stunden (Wecker | Timer)",
             "Stell einen (<time>){minutes} [($timer_partial_time)] Minuten (Wecker | Timer)",
             "Stell einen (<time>){seconds} [($timer_partial_time)] Sekunden (Wecker | Timer)",
         ]
     )
-    def set_timer(
-        self, hours: int = None, minutes: int = None, seconds: int = None, timer_partial_time=None
-    ):
-        human_timer_duration = self._set_timer(
-            "Der Timer {0} ist abgelaufen", hours, minutes, seconds, timer_partial_time
-        )
+    def set_timer(self, satellite_id, hours: int = None, minutes: int = None, seconds: int = None, timer_partial_time=None):
+        human_timer_duration = self._set_timer(satellite_id, "Der {0} Timer ist abgelaufen", hours, minutes, seconds, timer_partial_time)
         return f"Stelle Timer auf {human_timer_duration}"
-
-    @intents.sentences(["Lösche den letzten Timer"])
-    def delete_timer(self):
-        self.delete_timer()
-        return f"lösche Timer"
-
-
-        
