48706d2ce5367f460c4f6c5f9e92515419c83805
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2022-08-07 21:39:41 -0500

    Added an optional the to the sentences before the media_player name

--- home_intent/components/home_assistant/media_player/en.py
+++ home_intent/components/home_assistant/media_player/en.py
@@ -52,42 +52,42 @@ class MediaPlayer(BaseMediaPlayer):
         response = self._turn_on(media_player)
         return f"Turning {response['state']} the {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["turn down the volume of ($media_player)"])
+    @intents.sentences(["turn down the volume of [the] ($media_player)"])
     def volume_down(self, media_player):
         response = self._volume_down(media_player)
         return f"Turning down the volume of {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["turn up the volume of ($media_player)"])
+    @intents.sentences(["turn up the volume of [the] ($media_player)"])
     def volume_up(self, media_player):
         response = self._volume_up(media_player)
         return f"Turning up the volume of {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["mute the volume of ($media_player)"])
+    @intents.sentences(["mute the volume of [the] ($media_player)"])
     def volume_mute(self, media_player):
         response = self._volume_mute(media_player)
         return f"Muting the volume of {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["set the volume of ($media_player) to ($media_player_volume_level)"])
+    @intents.sentences(["set the volume of [the] ($media_player) to ($media_player_volume_level)"])
     def volume_set(self, media_player, media_player_volume_level):
         response = self._volume_set(media_player, media_player_volume_level)
         return f"Setting the volume level of {response['attributes']['friendly_name']} to {media_player_volume_level}"
 
-    @intents.sentences(["Play on ($media_player)"])
+    @intents.sentences(["Play on [the] ($media_player)"])
     def media_play(self, media_player):
         response = self._media_play(media_player)
         return f"Playback paused on {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["Pause on ($media_player)"])
+    @intents.sentences(["Pause on [the] ($media_player)"])
     def media_pause(self, media_player):
         response = self._media_pause(media_player)
         return f"Playback resumed on {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["Play next track on ($media_player)"])
+    @intents.sentences(["Play next track on [the] ($media_player)"])
     def media_next_track(self, media_player):
         response = self._media_next_track(media_player)
         return f"Playing next track on {response['attributes']['friendly_name']}"
 
-    @intents.sentences(["Play previous track on ($media_player)"])
+    @intents.sentences(["Play previous track on [the] ($media_player)"])
     def media_previous_track(self, media_player):
         response = self._media_previous_track(media_player)
         return f"Playing previous track on {response['attributes']['friendly_name']}"
